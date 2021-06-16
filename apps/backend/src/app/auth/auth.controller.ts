47818b0f016046d4941b823b66071951a1c3a6d3
Никита Петров <n.petrov@yclients.tech>
2021-06-17 00:31:59 +0300

    Add basic authorization (jwt) and registration

--- apps/backend/src/app/auth/auth.controller.ts
+++ apps/backend/src/app/auth/auth.controller.ts
@@ -1,16 +1,41 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
+import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { JwtAuthGuard } from './guards/jwt.guard';
 
 @Controller('auth')
 export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ) { }
+
+    @UsePipes(new ValidationPipe())
     @Post('register')
     async register(@Body() dto: AuthDto) {
+        const foundUser = await this.authService.findUser(dto.login);
 
+        if (foundUser) {
+            throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+        }
+
+        return this.authService.createUser(dto);
     }
 
+    @UsePipes(new ValidationPipe())
     @HttpCode(200)
     @Post('login')
-    async login(@Body() dto: AuthDto) {
+    async login(@Body() { login, password }: AuthDto) {
+        const { email } = await this.authService.validateUser(login, password);
 
+        return this.authService.login(email);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('test')
+    async testAuth() {
+        return {
+            isOk: true,
+        };
     }
 }
--- apps/backend/src/app/auth/auth.module.ts
+++ apps/backend/src/app/auth/auth.module.ts
@@ -1,19 +1,33 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { AuthController } from './auth.controller';
-import { AuthModel } from './auth.model';
+import { UserModel } from './user.model';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from '../../configs/jwt.config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     TypegooseModule.forFeature([
       {
-        typegooseClass: AuthModel,
+        typegooseClass: UserModel,
         schemaOptions: {
-          collection: 'Auth',
+          collection: 'User',
         }
       },
-    ])
+    ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
+    PassportModule,
+    ConfigModule,
   ],
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule { }
